3981183be22eeebd60926140029214d3ddb48085
Hadis Sulejmani <hadissulejmani@outlook.com>
2022-07-21 21:56:23 +0200

    added a new form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,53 @@
 import "./Create.css";
+import { useState } from "react";
+
 function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Create a recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Recipe method:</span>
+          <input
+            type="text"
+            value={method}
+            onChange={(e) => setMethod(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Recipe time:</span>
+          <textarea
+            type="number"
+            value={cookingTime}
+            onChange={(e) => setCookingTime(e.target.value)}
+            required
+          />
+        </label>
+        <button className="btn" type="submit">
+          Create
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Create;
